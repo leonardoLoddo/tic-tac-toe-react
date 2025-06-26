0941cd439bb58d21d63ae8052da0c3814d55277f
leonardoLoddo <20loddoloddo@gmail.com>
2025-06-26 11:52:20 +0200

    refactoring logica tabella e implementazione log

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  //gameTurns terrà traccia di che cella é stat premuta da che giocatore man mano che il gioco va avanti
+
   const [activePlayer, setActivePlayer] = useState("X");
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
-  }
   //creo lo state responsabile di tenere traccia del giocatore attivo
   //lo creo in app perché dovrá informare gameboard ma anche effettuare delle modifiche di stile qua in app
-  //creo la funzione per modificare il giocatore attivo, la passo come props a gameBoard in modo da potervi accedere da la
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+      // se l'array dei non é vuoto e nello scorso turno il giocatore era 'X' allora il giocatore corrente deve diventare 'O'
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      //inserisco i dati sull'ultimo turno svolto in testa all'array
+      //il turno é un oggetto che contiene le coordinate del quadrato premuto e il simbolo del giocatore che lo ha premuto
+
+      return updatedTurns;
+      //finalmente restituisco l'array aggiornato alla setter dello state
+    });
+  }
+  //creo la funzione per modificare il giocatore attivo e tenere traccia delle mosse della partita, la passo come props a gameBoard in modo da potervi accedere da la
+
   return (
     <main>
       <div id="game-container">
@@ -25,13 +49,10 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
-        {/* passo la funzione per modificare il giocatore attivo e il simbolo del giocatore attivo al momento a GameBoard */}
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {/* passo la funzione per modificare il giocatore attivo e l'array che tiene traccia dei turni */}
       </div>
-      LOG
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,29 +1,41 @@
-import { useState } from "react";
+// import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 //questo array rappresenta la tabella da gioco
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  function handleSelectSquare(rowIndex, colIndex) {
-    //se lo state contine un oggetto o array (mutabili) é fortemente consigliata laggiornamento in modo immutabile
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      // con lo spread operator vado a passare gli array interni come valori e non per riferimento
-      // faccio la stessa cosa per ogni array annidato utilizzando il metodo map sull'array padre
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      //eseguo la modifica alla copia dell'array andando a modificare la cella cliccata
-      return updatedBoard;
-      //restituisco a setGameBoard l'array agggiornato
-    });
 
-    onSelectSquare();
-    //ogni volta che un giocatore fa la sua mossa cambio il giocatore attivo
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+
+  for (const turn of turns) {
+    const { square, player } = turn; //da ciascun turno creo due costanti tramite destructuring: 'square' conterrá le coordinate della cella e 'player' il simbolo di chi la ha cliccata
+    const { row, col } = square; //con il destructuring, creo due costanti dove salvo la riga e la colonna di ogni cella
+
+    gameBoard[row][col] = player; //richiamo le costanti appena valorizzate per inserire nella giusta cella della tabella il giusto simbolo
   }
+
+  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  //   function handleSelectSquare(rowIndex, colIndex) {
+  //     //se lo state contine un oggetto o array (mutabili) é fortemente consigliata laggiornamento in modo immutabile
+  //     setGameBoard((prevGameBoard) => {
+  //       const updatedBoard = [
+  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //       ];
+  //       // con lo spread operator vado a passare gli array interni come valori e non per riferimento
+  //       // faccio la stessa cosa per ogni array annidato utilizzando il metodo map sull'array padre
+  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //       //eseguo la modifica alla copia dell'array andando a modificare la cella cliccata
+  //       return updatedBoard;
+  //       //restituisco a setGameBoard l'array agggiornato
+  //     });
+
+  //     onSelectSquare();
+  //     //ogni volta che un giocatore fa la sua mossa cambio il giocatore attivo
+  //   }
+
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -31,8 +43,8 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
-                  {/* passo una funzione anonima che faccia partire handleSelectSquare con i giusti parametri */}
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  {/* passo una funzione anonima che faccia partire onSelectSquare con i giusti parametri */}
                   {playerSymbol}
                 </button>
               </li>
